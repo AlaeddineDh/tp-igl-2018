b3258f497b053a30e71b2ada3563a17103a3e2d2
alaeddine <fa_dahamni@esi.dz>
2017-10-14 21:57:25 +0100

    added tests for split method

--- test/StringHelperTest.java
+++ test/StringHelperTest.java
@@ -1,8 +1,20 @@
+import org.junit.Test;
+
+import static org.junit.Assert.assertArrayEquals;
 
 /**
  * Created by FATEN on 13/10/2017.
  */
 public class StringHelperTest {
+
+    private StringHelper stringHelper = new StringHelper();
+
+    @Test
+    public void testSplit() {
+        String[] expected = new String[]{"str1", "str2", "str3"};
+        String[] actuals = stringHelper.split("str1,str2,str3", ',');
+        assertArrayEquals(expected, actuals);
+    }
 
 }
 
