a0487347fb1043147e58e5181c68f664efccf006
faten96 <32636018+faten96@users.noreply.github.com>
2017-10-15 10:30:21 +0200

    le test de la méthode éliminer

--- test/StringHelperTest.java
+++ test/StringHelperTest.java
@@ -1,3 +1,7 @@
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.ExpectedException;
+import static org.junit.Assert.*;
 
 /**
  * Created by FATEN on 13/10/2017.
@@ -12,6 +16,17 @@ public class StringHelperTest {
        String expected="table;chair;door;window";
 
        assertEquals(expected,s.Join(V,4));
+
+    }
+        
+    @Test
+    public void test_eliminate() throws Exception {
+
+        StringHelper s=new StringHelper();
+        String str="et je vais à la poste non ";
+        String expected=" je vais la poste ";
+
+        assertEquals(expected,s.Eliminate(str));
 
     }
 
