87c14fa5d2c9e8be86489d6a182f2eab5d031e3d
faten96 <32636018+faten96@users.noreply.github.com>
2017-10-15 10:17:29 +0200

    le test de la méthode join

--- test/StringHelperTest.java
+++ test/StringHelperTest.java
@@ -3,6 +3,17 @@
  * Created by FATEN on 13/10/2017.
  */
 public class StringHelperTest {
+        
+   @Test
+    public void test_join() throws Exception {
+
+       StringHelper s=new StringHelper();
+       String V[]=new String[]{"table","chair","door","window"};
+       String expected="table;chair;door;window";
+
+       assertEquals(expected,s.Join(V,4));
+
+    }
 
 }
 
