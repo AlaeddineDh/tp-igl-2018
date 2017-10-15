38d477437ac30c6193ac812f4754c350d6167cba
faten96 <32636018+faten96@users.noreply.github.com>
2017-10-15 10:46:02 +0200

    Test de la méhode occurrence ajouté + la déclaration de l'exception

--- test/StringHelperTest.java
+++ test/StringHelperTest.java
@@ -7,6 +7,9 @@ import static org.junit.Assert.*;
  * Created by FATEN on 13/10/2017.
  */
 public class StringHelperTest {
+    
+   @Rule
+    public final ExpectedException exception=ExpectedException.none();
         
    @Test
     public void test_join() throws Exception {
@@ -27,6 +30,15 @@ public class StringHelperTest {
         String expected=" je vais la poste ";
 
         assertEquals(expected,s.Eliminate(str));
+
+    }
+        
+    @Test
+    public void test_occurrence() throws Exception {
+        StringHelper s=new StringHelper();
+        String str="mot rien motIGL peu importemot mot phrase";
+
+        assertEquals(4,s.Occurrence("mot",str));
 
     }
 
