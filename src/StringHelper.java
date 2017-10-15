9ae95216a27d240c21a24e522652e3252d66212f
faten96 <32636018+faten96@users.noreply.github.com>
2017-10-15 10:14:44 +0200

    j'ai ajouté la méthode qui join les élèments d'un tableau

--- src/StringHelper.java
+++ src/StringHelper.java
@@ -3,5 +3,25 @@
  * Created by FATEN on 13/10/2017.
  */
 public class StringHelper {
+  
+    /**
+     * @param V le tableau qui contient les chaines de caractes
+     * @param length la taille de tableau
+     * @return une chaine qi contient les elements du tabeau entre eux un séparateur
+     */
+    public String Join(String V[],int length){
 
+        String result="";
+
+        for (int i=0;i<length-1;i++){
+            result=result+V[i]+";";
+        }
+        result=result+V[length-1];
+
+        return result;
+
+    }
+  
+  
+  
 }
