ea837d91a0c3c7a7baeb7fca3aaeb5a335e28795
faten96 <32636018+faten96@users.noreply.github.com>
2017-10-21 00:48:09 +0200

    SuiteClasse Join, Eliminer, Occurrene ajoutées

--- test/StringHelperTestSuite.java
+++ test/StringHelperTestSuite.java
@@ -4,7 +4,7 @@ import org.junit.runners.Suite;
 /**
  * Created by alaeddine on 10/18/17.
  */
-@Suite.SuiteClasses({StringHelperTest.class,CapitalizeTestParameterized.class,ApplyFormulaTestParameterized.class})
+@Suite.SuiteClasses({StringHelperTest.class,CapitalizeTestParameterized.class,ApplyFormulaTestParameterized.class,EliminerMotsVidesTestParameterized.class,OccurrenceTestParameterized.class,JoinTestParameterized.class})
 @RunWith(Suite.class)
 public class StringHelperTestSuite {
 }
